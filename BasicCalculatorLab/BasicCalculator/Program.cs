2785f552ce2ccd56c278dd7016e77fb127895ecb
SamuelMHarvey <59981663+SamuelMHarvey@users.noreply.github.com>
2020-01-28 15:59:48 -0600

    Update Program.cs

--- BasicCalculatorLab/BasicCalculator/Program.cs
+++ BasicCalculatorLab/BasicCalculator/Program.cs
@@ -8,27 +8,41 @@ namespace BasicCalculator
         {
             Console.WriteLine("*** WEATHER CALCULATOR ***");
 
+            // declare variables
+            string input;
+            int actualTemp;
+            int relativeHumidity;
+            int windSpeed;
+            int dewPoint;
+
+            // ask user for temp
+
             Console.WriteLine("Enter the temperature in Fahrenheit");
+            input = Console.ReadLine();
+            actualTemp = int.Parse(input);
 
-            // int.Parse will take a string data type and convert it to an int data type
-            int actualTemp = int.Parse(Console.ReadLine());
-
+            // ask user for relative humidity
             Console.WriteLine("Enter the relative humidity");
-            int relativeHumidity = int.Parse(Console.ReadLine());
+            input = Console.ReadLine();
+            relativeHumidity = int.Parse(input);
 
-            decimal dewPoint = actualTemp - 9m / 25m * (100 - relativeHumidity);
+            // calculate the dew point
+            dewPoint = actualTemp - 9 * (100 - relativeHumidity) / 25;
 
+            // print the dew point
             Console.WriteLine("The dew point is " + dewPoint + " degrees Fahrenheit");
-
-            //Wind Chill
-
+            
+            // ask for the wind speed
             Console.WriteLine("Enter the wind speed in miles per hour");
+            input = Console.ReadLine();
+            windSpeed = int.Parse(input);
 
-            int windSpeed = int.Parse(Console.ReadLine());
-
+            // calculate for the wind chill
             double windChill = 35.74 + 0.6215 * actualTemp - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * actualTemp * Math.Pow(windSpeed, 0.16);
 
+            // print the wind chill
             Console.WriteLine("The wind chill is " + Math.Round(windChill, 2) + " degrees Fahrenheit");
         }
+     
     }
 }
