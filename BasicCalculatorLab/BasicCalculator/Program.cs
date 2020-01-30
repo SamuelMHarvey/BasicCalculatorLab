3031ecc9a3cc1eacbd7c191dbeba21890ebf1839
SamuelMHarvey <59981663+SamuelMHarvey@users.noreply.github.com>
2020-01-30 15:47:20 -0600

    Update Program.cs

--- BasicCalculatorLab/BasicCalculator/Program.cs
+++ BasicCalculatorLab/BasicCalculator/Program.cs
@@ -13,32 +13,34 @@ namespace BasicCalculator
             int actualTemp;
             int relativeHumidity;
             int windSpeed;
-            int dewPoint;
 
             // ask user for temp
             actualTemp = GetIntegerInput("Enter the temperature in Fahrenheit");
 
             // bool isInt = int.TryParse(input, out actualTemp);
 
-            // ask user for relative humidity
-            relativeHumidity = GetIntegerInput("Enter the relative humidity");
+            if (actualTemp > 50)
+            {
+                // ask user for relative humidity
+                relativeHumidity = GetIntegerInput("Enter the relative humidity");
+                // Heat index equation HI = 0.5 * {T + 61.0 + [(T-68.0)*1.2] + (RH*0.094)}
+                double heatIndex = (0.5 * (actualTemp + 61 + (actualTemp - 68) * 1.2)) + (relativeHumidity * 0.094);
+                Console.WriteLine("The heat index is " + Math.Round(heatIndex, 2) + " degrees Fahrenheit");
+            }
+            else
+            {
+                // ask for the wind speed
+                Console.WriteLine("Enter the wind speed in miles per hour");
+                input = Console.ReadLine();
+                windSpeed = int.Parse(input);
 
-            // calculate the dew point
-            dewPoint = actualTemp - 9 * (100 - relativeHumidity) / 25;
+                // calculate for the wind chill
+                double windChill = 35.74 + 0.6215 * actualTemp - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * actualTemp * Math.Pow(windSpeed, 0.16);
 
-            // print the dew point
-            Console.WriteLine("The dew point is " + dewPoint + " degrees Fahrenheit");
-            
-            // ask for the wind speed
-            Console.WriteLine("Enter the wind speed in miles per hour");
-            input = Console.ReadLine();
-            windSpeed = int.Parse(input);
-
-            // calculate for the wind chill
-            double windChill = 35.74 + 0.6215 * actualTemp - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * actualTemp * Math.Pow(windSpeed, 0.16);
-
-            // print the wind chill
-            Console.WriteLine("The wind chill is " + Math.Round(windChill, 2) + " degrees Fahrenheit");
+                // print the wind chill
+                Console.WriteLine("The wind chill is " + Math.Round(windChill, 2) + " degrees Fahrenheit");
+            }
+     
         }
 
         // creating a method
